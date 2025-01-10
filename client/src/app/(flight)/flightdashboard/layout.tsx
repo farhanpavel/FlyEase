6488d8ce9f04c2234ba1ffee44d0719cc145e44f
farhanpavel <farhanpavel3@gmail.com>
2025-01-10 19:57:25 +0600

    Routing Problem fixed

--- client/src/app/(flight)/flightdashboard/layout.tsx
+++ client/src/app/(flight)/flightdashboard/layout.tsx
@@ -1,11 +1,29 @@
 "use client";
 import { FlightPanel } from "@/components/Flightpanel/page";
-
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 export default function Landing({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const id = Cookies.get("id");
+
+    if (!id) {
+      router.back();
+    } else {
+      setIsLoading(false);
+    }
+  }, [router]);
+
+  if (isLoading) {
+    return <div></div>;
+  }
   return (
     <div className="bg-[#F0F4F4] min-h-screen">
       <FlightPanel />
